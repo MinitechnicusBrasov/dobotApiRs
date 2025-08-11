bbb1df5c432a53be7f22aad9b35635ee9fd4b8cd
andreishark <andreishark10@gmail.com>
2025-08-11 12:45:13 +0300

    Updated metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,3 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-mod dobot;
+pub mod dobot;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
